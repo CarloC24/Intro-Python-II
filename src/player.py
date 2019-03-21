c5c4af20af59886e29962ae496b83f60c8a74f48
CarloC24 <carlo.clamucha@gmail.com>
2019-03-21 13:06:11 -0500

    added mvp

--- src/player.py
+++ src/player.py
@@ -1,13 +1,46 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from item_list import item_list
 
 
 class Player:
-    def __init__(self, startLocation):
+    def __init__(self,name, startLocation,items):
+        self.name = name
         self.location = startLocation
+        self.items = items
 
     def change_location(self, changedLocation):
         self.location = changedLocation
 
     def print_location(self):
-        print(self.location)
+        print(f'you are at {self.location.name}')
+
+    def print_items(self):
+        if len(self.items):
+            my_items = [x.name for x in self.items]
+            print(f'your items are {my_items}')
+        else:
+            print(f'you have no items in your inventory {self.name}')
+
+    def add_or_remove(self,item):
+        if item in [x.name for x in self.items]:
+            print(f'successfully removed {item} in your inventory in player.py')
+            self.items.remove(item_list[item])
+        else:
+             print(f'successfully added {item} in your inventory in player.py')
+             self.items.append(item_list[item])
+    def add_item(self,item):
+        if self.check_item(item):
+            print('player already has that item')
+        else:
+            self.items.append(item_list[item])
+    def remove_item(self,item):
+        if self.check_item(item):
+            self.items.remove(item_list[item])
+        else:
+            print(f'Cant remove item {item} because you dont have it')
+    def check_item(self,item):
+        if item in [x.name for x in self.items]:
+            return True
+        else:
+            return False
--- src/room.py
+++ src/room.py
@@ -1,11 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item_list import item_list
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description,items):
         self.name = name
         self.description = description
+        self.items = [items]
         self.n_to = None
         self.w_to = None
         self.e_to = None
@@ -20,3 +22,36 @@ class Room:
             return self.w_to
         if direction == 'e':
             return self.e_to
+
+    def print_items(self):
+        if self.items:
+             print([x.name for x in self.items])
+        else:
+            print('There are no items in the room')
+
+    def add_or_remove(self,item):
+        if self.check_item(self.items):
+            print(f'successfully removed {item} in the room')
+            self.items.remove(item_list[item])
+        else:
+             print(f'successfully added {item} in the room')
+             self.items.append(item_list[item])
+
+    def check_item(self,item):
+        if item in [x.name for x in self.items]:
+            return True
+        else:
+            return False
+    def add_item(self,item):
+        if self.check_item(item):
+            print(f'\n\n {item} does not exist on the inventory \n\n')
+        else:
+            print(f' \n\n successfully added {item} in your inventory \n\n')
+            self.items.append(item_list[item])
+    def remove_item(self,item):
+        if self.check_item(item):
+            self.items.remove(item_list[item])
+        else:
+            print(f'room does not have this item {item}')
+    def print_self(self):
+        print(self)
